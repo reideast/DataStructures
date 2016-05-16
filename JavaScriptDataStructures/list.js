9fa1a826e65d2ab1c8826c1768389b5be65d3ba8
Andrew Reid East <andrew@andreweast.net>
2016-05-16 02:31:07 +0100

    Change head and tail to pseudo-private members

--- JavaScriptDataStructures/list.js
+++ JavaScriptDataStructures/list.js
@@ -1,6 +1,8 @@
 ﻿function List(data) {
-    this.head = null;
-    this.tail = null;
+    // note: these are not actually private properties. but, JS isn't designed to do private (or more importantly, protected) members. see: http://philipwalton.com/articles/implementing-private-and-protected-members-in-javascript/
+    // so, pseudo-private members will have to do
+    this._head = null;
+    this._tail = null;
 
     if (arguments.length > 0) {
         for (var i = 0; i < arguments.length; ++i) {
@@ -16,45 +18,42 @@
         // no data passed
         // leave head/tail as null
     //}
-
-
 }
-//Object.defineProperties(List, {
-//    head: { get: function () { return head; } },
-//    tail: { get: function () { return tail; } }
-//});
+
+Object.defineProperty(List.prototype, "getHead", { get: function () { return this._head; } });
+Object.defineProperty(List.prototype, "getTail", { get: function () { return this._tail; } });
 
 List.prototype.append = function (item) {
     console.log("append(), item=" + item);
-    if (this.head === null) {
-        this.head = this.tail = new Node(item);
+    if (this._head === null) {
+        this._head = this._tail = new Node(item);
     } else {
-        this.tail.next = new Node(item, this.tail, null);
-        this.tail = this.tail.next;
+        this._tail.next = new Node(item, this._tail, null);
+        this._tail = this._tail.next;
     }
 }
 List.prototype.appendList = function (list) {
-    if (this.head === null) {
-        this.head = list.head;
-        this.tail = list.tail;
+    if (this._head === null) {
+        this._head = list.getHead;
+        this._tail = list.getTail;
     } else {
-        this.tail.next = list.head;
-        this.tail = list.tail;
+        this._tail.next = list.getHead;
+        this._tail = list.getTail;
     }
 }
 List.prototype.prepend = function (item) {
-    if (this.head === null) {
-        this.head = this.tail = new Node(item);
+    if (this._head === null) {
+        this._head = this._tail = new Node(item);
     } else {
-        this.head.prev = new Node(item, null, this.head);
-        this.head = this.head.prev;
+        this._head.prev = new Node(item, null, this._head);
+        this._head = this._head.prev;
     }
 }
 
 List.prototype.toString = function () {
     // not going to do a customer StringBuilder since there's no performance gain in every browser except IE6/7: http://www.sitepoint.com/javascript-fast-string-concatenation/
     var s = "";
-    var curr = this.head;
+    var curr = this._head;
     while (curr !== null) {
         s += curr.data + (curr.next !== null ? "," : "");
         curr = curr.next;
