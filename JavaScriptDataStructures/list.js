869de23a025421949b8b656239ac7e938eccff39
Andrew Reid East <andrew@andreweast.net>
2016-05-16 22:01:45 +0100

    Implement List.length(), elementAt(), indexOf(), each(), and some()

--- JavaScriptDataStructures/list.js
+++ JavaScriptDataStructures/list.js
@@ -1,4 +1,9 @@
-﻿function List(data) {
+﻿// Define a linked-list data structure
+// double-linked, head and tail pointers
+// Constructor can take a variable number of arguments
+// All arguments that are Arrays will have their elements added (recursively!)
+// All other arguments will be added as single list nodes
+function List(data) {
     // note: these are not actually private properties. but, JS isn't designed to do private (or more importantly, protected) members. see: http://philipwalton.com/articles/implementing-private-and-protected-members-in-javascript/
     // so, pseudo-private members will have to do
     this._head = null;
@@ -8,7 +13,7 @@
         for (var i = 0; i < arguments.length; ++i) {
             if (Array.isArray(arguments[i])) {
                 for (var j = 0; j < arguments[i].length; ++j) {
-                    this.appendList.call(this, new List(arguments[i][j]));
+                    this.concat.call(this, new List(arguments[i][j]));
                 }
             } else {
                 this.append(arguments[i]);
@@ -23,6 +28,17 @@
 Object.defineProperty(List.prototype, "getHead", { get: function () { return this._head; } });
 Object.defineProperty(List.prototype, "getTail", { get: function () { return this._tail; } });
 
+List.prototype.toString = function () {
+    // not going to do a customer StringBuilder since there's no performance gain in every browser except IE6/7: http://www.sitepoint.com/javascript-fast-string-concatenation/
+    var s = "";
+    var curr = this._head;
+    while (curr !== null) {
+        s += curr.data + (curr.next !== null ? "," : "");
+        curr = curr.next;
+    }
+    return s;
+};
+
 List.prototype.append = function (item) {
     console.log("append(), item=" + item);
     if (this._head === null) {
@@ -32,7 +48,7 @@ List.prototype.append = function (item) {
         this._tail = this._tail.next;
     }
 }
-List.prototype.appendList = function (list) {
+List.prototype.concat = function (list) {
     if (this._head === null) {
         this._head = list.getHead;
         this._tail = list.getTail;
@@ -49,14 +65,71 @@ List.prototype.prepend = function (item) {
         this._head = this._head.prev;
     }
 }
-
-List.prototype.toString = function () {
-    // not going to do a customer StringBuilder since there's no performance gain in every browser except IE6/7: http://www.sitepoint.com/javascript-fast-string-concatenation/
-    var s = "";
+List.prototype.length = function () {
+    var count = 0;
     var curr = this._head;
     while (curr !== null) {
-        s += curr.data + (curr.next !== null ? "," : "");
+        ++count;
         curr = curr.next;
     }
-    return s;
-};
+    return count;
+}
+List.prototype.elementAt = function (position) {
+    var i = 0;
+    var curr = this._head;
+    while (curr !== null) {
+        if (i++ === position)
+            return curr.data;
+        curr = curr.next;
+    }
+    throw new RangeError("Index out of range.");
+}
+List.prototype.indexOf = function (data) {
+    var i = 0;
+    var curr = this._head;
+    while (curr !== null) {
+        if (curr.data === data)
+            return i;
+        ++i;
+        curr = curr.next;
+    }
+    return -1;
+}
+List.prototype.lastIndexOf = function (data) {
+    var i = 0;
+    var found = -1;
+    var curr = this._head;
+    while (curr !== null) {
+        if (curr.data === data)
+            found = i;
+        ++i;
+        curr = curr.next;
+    }
+    return found;
+}
+List.prototype.every = function (callback, thisArg) {
+    if (arguments.length < 2)
+        thisArg = this;
+    var i = 0;
+    var curr = this._head;
+    while (curr !== null) {
+        if (!callback.call(thisArg, curr.data, i, this))
+            return false;
+        ++i;
+        curr = curr.next;
+    }
+    return true;
+}
+List.prototype.some = function (callback, thisArg) {
+    if (arguments.length < 2)
+        thisArg = this;
+    var i = 0;
+    var curr = this._head;
+    while (curr !== null) {
+        if (callback.call(thisArg, curr.data, i, this))
+            return true;
+        ++i;
+        curr = curr.next;
+    }
+    return false;
+}
--- JavaScriptDataStructures/testList.js
+++ JavaScriptDataStructures/testList.js
@@ -10,15 +10,29 @@ var list = new List(0, arr);
 test.log("list = " + list);
 
 container.appendChild(test.elem = document.createElement("p"));
-list.append(4);
-test.log("Append (4): " + list);
+list.append(3);
+test.log("Append (3): " + list);
 
 container.appendChild(test.elem = document.createElement("p"));
 list.prepend(-1);
 test.log("Prepend (-1): " + list);
 
 container.appendChild(test.elem = document.createElement("p"));
+test.log("length: " + list.length());
+
 container.appendChild(test.elem = document.createElement("p"));
+test.log("elementAt: [0]=" + list.elementAt(0) + " [5]=" + list.elementAt(5) + " [2]=" + list.elementAt(2));
+try {
+    test.log(" [-1]=");
+    test.log(list.elementAt(-1));
+} catch (e) {
+    test.log(e.message);
+}
+
 container.appendChild(test.elem = document.createElement("p"));
+test.log("indexOf: -1=" + list.indexOf(-1) + " 0=" + list.indexOf(0) + "10=" + list.indexOf(10) + "'blah'=" + list.indexOf('blah') + " 3=" + list.indexOf(3));
+
 container.appendChild(test.elem = document.createElement("p"));
+test.log("lastIndexof: -1=" + list.lastIndexOf(-1) + " 0=" + list.lastIndexOf(0) + " 10=" + list.lastIndexOf(10) + "'blah'=" + list.indexOf('blah') + " 3=" + list.lastIndexOf(3));
+
 container.appendChild(test.elem = document.createElement("p"));
