7774055607ae471f8893cc3626b38972a9cfc8d3
Andrew Reid East <andrew@andreweast.net>
2016-05-16 01:37:51 +0100

    Create 1-level recursive toString() for Node

--- JavaScriptDataStructures/node.js
+++ JavaScriptDataStructures/node.js
@@ -2,5 +2,8 @@
     this.data = data;
     this.prev = prev || null;
     this.next = next || null;
-    console.log("a node was created: " + this.data + this.prev + this.next);
+    console.log("node created: " + this.toString());
 }
+Node.prototype.toString = function () {
+    return "NODE{" + (this.prev === null ? null : "NODE{"+this.prev.data+"}") + " << (" + this.data + ") >> " + (this.next === null ? null : "NODE{"+this.next.data+"}") + "}";
+};
